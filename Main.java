0ae11008781f2ee98b275365aa408e6e7f189220
HITARTH3452 <112773447+HITARTH3452@users.noreply.github.com>
2022-09-06 11:39:01 +0530

    Add files via upload
    DAY 1 PROBLEMS

---
+++ Main.java
@@ -0,0 +1,50 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+//  prob 1
+        System.out.println("Hello I am enjoying coding");
+
+//  prob 2
+        System.out.println("Hello");
+        System.out.println("I");
+        System.out.println("am");
+        System.out.println("enjoying");
+        System.out.println("coding");
+
+//  prob 3
+        System.out.println(100 * 8 * 3);
+        System.out.println(100 + 8 + 3);
+
+//  prob 4
+        System.out.println(100 * 40);
+
+//  prob 5
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+
+//  prob 6
+        System.out.println("* *");
+        System.out.println("* *");
+        System.out.println("* *");
+        System.out.println("* *");
+        System.out.println("* *");
+        System.out.println("****");
+
+//  prob 7
+        System.out.println(987653%10);
+
+//  prob 8
+        int m = 15;
+        int n = 4;
+        System.out.println(m+n);
+        System.out.println(m*n);
+        System.out.println(m-n);
+        
+    }
+}
+
